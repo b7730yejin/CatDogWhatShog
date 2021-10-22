94e00b9d403012574ebbc8ee04c62554fbe3aec1
b7730yejin <83448867+b7730yejin@users.noreply.github.com>
2021-10-22 19:21:53 +0900

    Delete member directory

--- member/AdminService.java
+++
@@ -1,6 +0,0 @@
-package com.demo.cdmall1.domain.member.service;
-
-
-public class AdminService {
-
-}
--- member/Level.java
+++
@@ -1,5 +0,0 @@
-package com.demo.cdmall1.domain.member.entity;
-
-public enum Level {
-	BRONZE, SILVER, GOLD;
-}
